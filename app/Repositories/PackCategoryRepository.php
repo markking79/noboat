d68618731af93d8446d2cba9250df258d44f9b1e
Mark King <mark@markking.org>
2019-07-11 01:39:07 -0500

    added redis cache

--- app/Repositories/PackCategoryRepository.php
+++ app/Repositories/PackCategoryRepository.php
@@ -3,11 +3,18 @@
 namespace App\Repositories;
 
 use App\PackCategory;
+use Illuminate\Support\Facades\Cache;
 
 class PackCategoryRepository implements PackCategoryRepositoryInterface
 {
+    private $secondsCache = 60;//60*60*24;
+
     public function getAll ()
     {
-        return PackCategory::orderBy ('weight')->get ();
+        $data = Cache::tags('pack_categories')->remember('pack_categories', $this->secondsCache, function () {
+            return PackCategory::orderBy ('weight')->get ();
+        });
+
+        return $data;
     }
 }
--- app/Repositories/PackRepository.php
+++ app/Repositories/PackRepository.php
@@ -3,9 +3,13 @@
 namespace App\Repositories;
 
 use App\Pack;
+use Illuminate\Support\Facades\Cache;
 
 class PackRepository implements PackRepositoryInterface
 {
+
+    private $secondsCache = 60;//60*60*24;
+
     public function getAll ()
     {
 
@@ -18,6 +22,12 @@ class PackRepository implements PackRepositoryInterface
 
     public function getByIdWithAllPackItems ($id)
     {
-        return Pack::where ('id', $id)->with (['items'])->first ();
+
+        $data = Cache::tags('pack-'.$id)->remember('pack-'.$id, $this->secondsCache, function () use ($id) {
+            return Pack::where ('id', $id)->with (['user', 'items'])->first ();
+        });
+
+        return $data;
+
     }
 }
