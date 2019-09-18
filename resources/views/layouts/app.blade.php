56f384ae20e6305927eda55dd8d11dd236ef0c73
Mark King <mark@markking.org>
2019-09-18 18:39:05 -0500

    added image width and height for facebook share

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,8 +18,8 @@
     <meta property="og:type" content="website" />
     <meta property="og:title" content="{{ config('app.name') }}" />
     <meta property="og:image" content="{{config('app.url')}}/images/packs/facebook-share.jpg" />
-    <meta property="og:image:width" content="1200" />
-    <meta property="og:image:height" content="630" />
+    <meta property="og:image:width" content="1200px" />
+    <meta property="og:image:height" content="630px" />
 </head>
 <body>
     <div id="app">
